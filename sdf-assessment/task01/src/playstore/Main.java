ad83ba3933319a7ca8fbbdbada8ff9d3da5f3045
CevieC <148834098+CevieC@users.noreply.github.com>
2023-11-16 17:16:08 +0800

    final!

--- sdf-assessment/task01/src/playstore/Main.java
+++ sdf-assessment/task01/src/playstore/Main.java
@@ -5,11 +5,11 @@ public class Main{
     public static void main(String[] args) throws Exception {
 
         // check if the csv filename exists in the parameter
-        // if(args.length <= 0) {
-        //     System.err.println("No CSV File found, try again.");
-        //     System.exit(1);
-        // }
-        PlayStore.getMap("sdf-assessment\\task01\\googleplaystore.csv");
+        if(args.length <= 0) {
+            System.err.println("No CSV File found, try again.");
+            System.exit(1);
+        }
+        PlayStore.getMap(args[0]);
         PlayStore.sortCategory();
         PlayStore.display();
     }
